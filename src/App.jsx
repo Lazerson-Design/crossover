7a3d4fc3702c5c91779c8787c9c9d248f76c5c04
basel-abdelazim <basel.abdelazim@gmail.com>
2024-09-16 15:49:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import Footer from "./components/Footer";
 import ContactForm from "./components/Contactform";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
 import Hero from "./components/Hero";
-
 function App() {
   return (
-    <div className="App" style={bodyStyle}>
+    <div className="App">
       <Navbar />
       <Hero />
+      <About />
+      <Gallery />
       <ContactForm />
       <Footer />
     </div>
   );
 }
 
-const bodyStyle = {
-  margin: 0,
-};
 
 export default App;
