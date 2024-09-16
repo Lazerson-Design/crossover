5051abbeee481525af4d50de4ddf729532ab98df
Albert Lazerson <albert@lazerson-design.de>
2024-09-16 14:38:55 +0200

    navbar in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "./components/Footer";
 import ContactForm from "./components/Contactform";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <ContactForm />
       <Footer />
     </div>
