2cca7bec9172e134c9442843d5965745755397c8
basel-abdelazim <basel.abdelazim@gmail.com>
2024-09-16 11:42:41 +0200

    add footer App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react';
+import Footer from './components/Footer';
+import ContactForm from './components/Contactform';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="App">
+      <ContactForm />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
